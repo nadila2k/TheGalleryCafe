cbbe5837d10c66e51277cd95fb73d04e13400efd
nadila2k <146557116+nadila2k@users.noreply.github.com>
2024-07-01 10:20:20 +0530

    foooter is done

--- Client/partials/footer.php
+++ Client/partials/footer.php
@@ -1,67 +1,60 @@
 <footer id="footer">
     <div class="footer-top">
-      <div class="container">
-        <div class="row">
-        
-        <div class="row mt-5">
+        <div class="container" id="contact">
 
-<div class="col-lg-4">
-    <div class="info">
-        <div class="address">
-            <i class="bi bi-geo-alt"></i>
-            <h4>Location:</h4>
-            <p>A108 Adam Street, New York, NY 535022</p>
+            
+                <div class="info">
+                    <div class="address">
+                        <i class="bi bi-geo-alt"></i>
+                        <h4>Location:</h4>
+                        <p>A108 Adam Street, New York, NY 535022</p>
+                    </div>
+
+                    <div class="open-hours">
+                        <i class="bi bi-clock"></i>
+                        <h4>Open Hours:</h4>
+                        <p>
+                            Monday-Saturday:<br>
+                            11:00 AM - 2300 PM
+                        </p>
+                    </div>
+
+                    <div class="email">
+                        <i class="bi bi-envelope"></i>
+                        <h4>Email:</h4>
+                        <p>info@example.com</p>
+                    </div>
+
+                    <div class="phone">
+                        <i class="bi bi-phone"></i>
+                        <h4>Call:</h4>
+                        <p>+1 5589 55488 55s</p>
+                    </div>
+
+                
+
+            
+
         </div>
 
-        <div class="open-hours">
-            <i class="bi bi-clock"></i>
-            <h4>Open Hours:</h4>
-            <p>
-                Monday-Saturday:<br>
-                11:00 AM - 2300 PM
-            </p>
-        </div>
-
-        <div class="email">
-            <i class="bi bi-envelope"></i>
-            <h4>Email:</h4>
-            <p>info@example.com</p>
-        </div>
-
-        <div class="phone">
-            <i class="bi bi-phone"></i>
-            <h4>Call:</h4>
-            <p>+1 5589 55488 55s</p>
-        </div>
 
     </div>
-
-</div>
-
-</div>
-
-</div>
-
-        </div>
-      </div>
     </div>
 
     <div class="container">
-      <div class="copyright">
-        &copy; Copyright <strong><span>Restaurantly</span></strong>. All Rights Reserved
-      </div>
-      <div class="credits">
-        <!-- All the links in the footer should remain intact. -->
-        <!-- You can delete the links only if you purchased the pro version. -->
-        <!-- Licensing information: https://bootstrapmade.com/license/ -->
-        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/restaurantly-restaurant-template/ -->
-        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-      </div>
+        <div class="copyright">
+            &copy; Copyright <strong><span>Restaurantly</span></strong>. All Rights Reserved
+        </div>
+        <div class="credits">
+   
+            Designed by <a href="">NadilaNawod</a>
+        </div>
     </div>
-  </footer>
+</footer>
 
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.3/js/bootstrap.bundle.min.js"></script>
 <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
 </body>
+
 </html>
